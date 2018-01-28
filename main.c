e75bd074904fba9afd89edfa35477410029b40e1
ShrinathN <shrinath.nimare@gmail.com>
2018-01-28 14:37:34 +0530

    Added comments

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/*  Copyright (C) 2018 Shrinath Nimare
+    This file is part of ps1avr
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+//TODO
+//1.fix random reset problem
 #define F_CPU 16000000UL
 #include </usr/avr/include/avr/io.h>
 #include </usr/avr/include/util/delay.h>
@@ -5,48 +20,50 @@
 #include </usr/avr/include/avr/interrupt.h>
 #include "psx_controller.h"
 
-void uart_init();
-void uart_send(BYTE data);
-void WDT_off(void);
+void uart_init();           //for initializing the UART
+void uart_send(BYTE data);  //sending a single byte through UART
+//void WDT_off(void);
 void main()
 {
 
-    DDRC = 1;
+    DDRC = 1;           //to blink the LED at PC0
     PORTC = 1;
     _delay_ms(500);
     PORTC = 0;
-    uart_init();
-    WDT_off();
-    psx_bus_init();
-    int d;
+
+    uart_init();        //initializing UART
+
+    psx_bus_init();     //initializing PSX I/O pins
+    int keysPessed;
     while(1)
     {
-        psx_read_state(&d);
-//        _delay_ms(2);
-        uart_send(d&0xff);
-        uart_send((d>>8)&0xff);
+	psx_read_state(&keysPessed);    //reads the data
+	_delay_ms(2);                   //can be commented for faster operation
+	uart_send(keysPessed&0xff);              //sends the lower byte aka 4th byte
+	uart_send((keysPessed>>8)&0xff);         //send the upper byte aka 5th byte
     }
 }
 
 void uart_init()
 {
-    UCSRB |= (1 << TXEN);
-    UBRRL = 103;
-    UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);
+    UCSRB |= (1 << TXEN);       //enables TX
+    UBRRL = 103;                //BAUD rate at 9600
+    UCSRC |= (1 << URSEL) |     //selecting UCSRC (NOT UBRRH)
+	    (1 << UCSZ0) |      //setting as 8 bits
+	    (1 << UCSZ1);
 }
 
 void uart_send(BYTE data)
 {
-    while((UCSRA & (1 << UDRE)) == 0){};
-    UDR = data;
+    while((UCSRA & (1 << UDRE)) == 0){};    //while UDR is not empty, wait
+    UDR = data;                             //send data
 }
 
+
+/*
 void WDT_off(void)
 {
-    /* reset WDT */
-//    _WDR();
-    /* Write logical one to WDCE and WDE */
     WDTCR |= (1<<WDCE) | (1<<WDE);
-    /* Turn off WDT */
     WDTCR = 0x00;
 }
+*/
--- psx_controller.h
+++ psx_controller.h
@@ -1,3 +1,16 @@
+/*  Copyright (C) 2018 Shrinath Nimare
+    This file is part of ps1avr
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 //pin definitions
 #define PSX_DIRECTION DDRB
 #define PSX_PORT PORTB
@@ -12,127 +25,147 @@
 typedef unsigned char BYTE;
 
 //initializes the AVR pins
-void psx_bus_init()
+void psx_bus_init()		//function to initialize the PSX bus pins
 {
-    PSX_DIRECTION =
-            (1 << PSX_CLK) |
-            (1 << PSX_ATT) |
-            (1 << PSX_CMD);
+    PSX_DIRECTION =		//enabling CLK, ATT, and CMD pins as out
+	    (1 << PSX_CLK) |
+	    (1 << PSX_ATT) |
+	    (1 << PSX_CMD);
     PSX_PORT =
-            (1 << PSX_DATA) |
-            (1 << PSX_CLK) |
-            (1 << PSX_ATT) |
-            (1 << PSX_CMD) |
-            (1 << PSX_ACK);
+	    (1 << PSX_CLK) |	//setting CLK, ATT and CMD as high,
+	    (1 << PSX_ATT) |
+	    (1 << PSX_CMD) |
+	    (1 << PSX_DATA) |	//turning on DATA and ACK pins' internal pull down
+	    (1 << PSX_ACK);
 }
 
-void psx_att_low()
+void psx_att_low()		//to set ATT low
 {
     PSX_PORT &= ~(1 << PSX_ATT);
 }
 
-void psx_att_high()
+void psx_att_high()		//to set ATT high
 {
     PSX_PORT |= (1 << PSX_ATT);
 }
 
-void psx_clk_low()
+void psx_clk_low()		//to set CLK low
 {
     PSX_PORT &= ~(1 << PSX_CLK);
 }
 
-void psx_clk_high()
+void psx_clk_high()		//to set CLK high
 {
     PSX_PORT |= (1 << PSX_CLK);
 }
 
-void psx_read_state(int * data)
+void psx_read_state(int * data)	//to read the button data from a controller
 {
-    BYTE cmd, counter;
+    BYTE command, counter;
 
-    psx_att_low();                      //ATT low to enable controller
+    psx_att_low();				//ATT low to enable controller
 
     //byte 1
-    PSX_PORT |= (1 << PSX_CMD);
-    cmd = 0x01;                         //first command to init communication
-    _delay_us(2);                       //2us delay
-    for(counter = 0; counter < 8; counter++)
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    command = 0x01;				//first command to init communication
+    _delay_us(2);			        //2us delay
+    for(counter = 0; counter < 8; counter++)	//loop to read a byte from the controller
     {
-        PSX_PORT &= ~(1 << PSX_CMD);
-        PSX_PORT |= ((cmd >> counter)&0x1)<<PSX_CMD;
-        psx_clk_low();                      //CLK low for full duration because we dont need to read anything on 0x1
-        _delay_us(PSX_CLK_DELAY_US); //delay
-        psx_clk_high();
-        _delay_us(PSX_CLK_DELAY_US); //delay
+	PSX_PORT &= ~(1 << PSX_CMD);		//clearing past CMD pin state
+	PSX_PORT |=				//pushing bit to CMD pin
+		((command >> counter)
+		 &0x1)
+		<<PSX_CMD;
+	psx_clk_low();				//CLK low period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
+	psx_clk_high();				//CLK high period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
     }
-    PSX_PORT |= (1 << PSX_CMD);
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
 
-    _delay_us(PSX_BYTE_DELAY_US);
+    _delay_us(PSX_BYTE_DELAY_US);		//delay between bytes
 
     //byte 2
-    cmd = 0x42;
-    PSX_PORT |= (1 << PSX_CMD);
-    for(counter = 0; counter < 8; counter++)
+    command = 0x42;				//command 0x42 to request data
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    for(counter = 0; counter < 8; counter++)	//loop to read a byte from the controller
     {
-        PSX_PORT &= ~(1 << PSX_CMD);
-        PSX_PORT |= ((cmd >> counter)&0x1)<<PSX_CMD;
-        psx_clk_low();
-        _delay_us(PSX_CLK_DELAY_US); //delay
-        psx_clk_high();
-        _delay_us(PSX_CLK_DELAY_US); //delay
+	PSX_PORT &= ~(1 << PSX_CMD);		//clearing past CMD pin state
+	PSX_PORT |=				//pushing bit to CMD pin
+		((command >> counter)
+		 &0x1)
+		<<PSX_CMD;
+	psx_clk_low();				//CLK low period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
+	psx_clk_high();				//CLK high period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
     }
-    PSX_PORT |= (1 << PSX_CMD);
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
 
-    _delay_us(PSX_BYTE_DELAY_US);
+    _delay_us(PSX_BYTE_DELAY_US);		//delay between bytes
 
     //byte 3
-    cmd = 0;
-    PSX_PORT |= (1 << PSX_CMD);
-    for(counter = 0; counter < 8; counter++)
+    command = 0;				//no command
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    for(counter = 0; counter < 8; counter++)	//loop to read a byte from the controller
     {
-        PSX_PORT &= ~(1 << PSX_CMD);
-        PSX_PORT |= ((cmd >> counter)&0x1)<<PSX_CMD;
-        psx_clk_low();
-        _delay_us(PSX_CLK_DELAY_US); //delay
-        psx_clk_high();
-        _delay_us(PSX_CLK_DELAY_US); //delay
+	PSX_PORT &= ~(1 << PSX_CMD);		//clearing past CMD pin state
+	PSX_PORT |=				//pushing bit to CMD pin
+		((command >> counter)
+		 &0x1)
+		<<PSX_CMD;
+	psx_clk_low();				//CLK low period
+	_delay_us(PSX_CLK_DELAY_US); //delay
+	psx_clk_high();				//CLK high period
+	_delay_us(PSX_CLK_DELAY_US); //delay
     }
-    PSX_PORT |= (1 << PSX_CMD);
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
 
-    _delay_us(PSX_BYTE_DELAY_US);
+    _delay_us(PSX_BYTE_DELAY_US);		//delay between bytes
 
     //byte 4
     *data = 0;
-    cmd = 0;
-    PSX_PORT |= (1 << PSX_CMD);
-    for(counter = 0; counter < 8; counter++)
+    command = 0;
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    for(counter = 0; counter < 8; counter++)	//loop to read a byte from the controller
     {
-        PSX_PORT &= ~(1 << PSX_CMD);
-        PSX_PORT |= ((cmd >> counter)&0x1)<<PSX_CMD;
-        psx_clk_low();
-        _delay_us(PSX_CLK_DELAY_US); //delay
-        *data |= ((~PSX_PIN & (1 << PSX_DATA))>>4) << counter;
-        psx_clk_high();
-        _delay_us(PSX_CLK_DELAY_US); //delay
+	PSX_PORT &= ~(1 << PSX_CMD);		//clearing past CMD pin state
+	PSX_PORT |=				//pushing bit to CMD pin
+		((command >> counter)
+		 &0x1)
+		<<PSX_CMD;
+	psx_clk_low();				//CLK low period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
+	*data |= ((~PSX_PIN &			//setting the lower byte as 4th byte serial data
+		   (1 << PSX_DATA))
+		  >>PSX_DATA)
+		<< (counter);
+	psx_clk_high();				//CLK high period
+	_delay_us(PSX_CLK_DELAY_US); //delay
     }
-    PSX_PORT |= (1 << PSX_CMD);
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
 
-    _delay_us(PSX_BYTE_DELAY_US);
+    _delay_us(PSX_BYTE_DELAY_US);		//delay between bytes
 
     //byte 5
-    cmd = 0;
-    PSX_PORT |= (1 << PSX_CMD);
-    for(counter = 0; counter < 8; counter++)
+    command = 0;
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    for(counter = 0; counter < 8; counter++)	//loop to read a byte from the controller
     {
-        PSX_PORT &= ~(1 << PSX_CMD);
-        PSX_PORT |= ((cmd >> counter)&0x1)<<PSX_CMD;
-        psx_clk_low();
-        _delay_us(PSX_CLK_DELAY_US); //delay
-        *data |= ((~PSX_PIN & (1 << PSX_DATA))>>4) << (counter + 8);
-        psx_clk_high();
-        _delay_us(PSX_CLK_DELAY_US); //delay
+	PSX_PORT &= ~(1 << PSX_CMD);		//clearing past CMD pin state
+	PSX_PORT |=				//pushing bit to CMD pin
+		((command >> counter)
+		 &0x1)
+		<<PSX_CMD;
+	psx_clk_low();				//CLK low period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
+	*data |= ((~PSX_PIN &			//setting the lower byte as 4th byte serial data
+		   (1 << PSX_DATA))
+		  >>PSX_DATA)
+		<< (counter + 8);
+	psx_clk_high();				//CLK high period
+	_delay_us(PSX_CLK_DELAY_US);		//delay
     }
-    PSX_PORT |= (1 << PSX_CMD);
-
-    psx_att_high();
+    PSX_PORT |= (1 << PSX_CMD);			//setting CMD pin default high
+    psx_att_high();				//ATT high again to disable comm
 }
